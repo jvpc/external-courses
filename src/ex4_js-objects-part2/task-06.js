1cf9782db3f0cea050428dc8535bf7cedeececb4
jvpc <pure01@mail.ru>
2018-10-30 22:42:44 +0300

    complete

---
+++ src/ex4_js-objects-part2/task-06.js
@@ -0,0 +1,10 @@
+'use strict'
+function titleSymbol (str) {
+  var arr = str.split(' ');
+  var i = 0;
+  for (i; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+  }
+  return arr.join(' ');
+};
+module.exports = titleSymbol;
