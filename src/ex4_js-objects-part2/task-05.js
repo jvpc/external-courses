f58b6789f62ace161db238bc8161a115ab3201da
Tatyankin Constantine <43969052+jvpc@users.noreply.github.com>
2018-10-31 11:33:52 +0300

    Delete task-05.js

--- src/ex4_js-objects-part2/task-05.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-function findString(str1, str2) {
-    if (!str1) return false;
-    if (~str1.indexOf(str2)) {
-        return true;
-    }
-    return false;
-};
-module.exports = findString;
