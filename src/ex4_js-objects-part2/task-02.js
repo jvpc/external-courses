75b307ed68fb04744a2493d1196ab5f5236cec51
Tatyankin Constantine <43969052+jvpc@users.noreply.github.com>
2018-10-31 11:33:35 +0300

    Delete task-02.js

--- src/ex4_js-objects-part2/task-02.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-var withoutPrototype;
-function createObjWithoutPrototype() {
-    withoutPrototype = Object.create(null);
-    return withoutPrototype;
-};
-module.exports = createObjWithoutPrototype;
