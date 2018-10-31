7e299670fc6a41668620f9b275e8f286f3c2d1e9
Tatyankin Constantine <43969052+jvpc@users.noreply.github.com>
2018-10-31 11:40:10 +0300

    Update task-06.js

--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -1,7 +1,6 @@
 'use strict'
 function titleSymbol (str) {
-  var arr = str.split(' ');
-  var i = 0;
+  var arr = str.split(' '), i = 0;
   for (i; i < arr.length; i++) {
     arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
   }
