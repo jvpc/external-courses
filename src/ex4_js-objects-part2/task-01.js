a6f773a95ebf63e9015873c1c416657f13e71f16
Tatyankin Constantine <43969052+jvpc@users.noreply.github.com>
2018-10-31 11:33:29 +0300

    Delete task-01.js

--- src/ex4_js-objects-part2/task-01.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-function findPropertyInPrototype (nameProperty, obj) {
-    if (!obj.hasOwnProperty(nameProperty) && nameProperty in obj) {
-      return obj[nameProperty];
-    }
-    return undefined;
-};
-module.exports = findPropertyInPrototype;
